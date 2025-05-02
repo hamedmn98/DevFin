c4a3389fb950f847c3f90c4764b8e0bd097b5a66
hamed mn <hamedmoshfeghi7@gmail.com>
2025-05-02 15:32:57 +0330

    mvc app added

---
+++ DevFinProject/DevFin_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevFin_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
