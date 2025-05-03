3529fc0b896d25e2146311dd70f92c04cd1c7ee6
hamed mn <hamedmoshfeghi7@gmail.com>
2025-05-03 13:55:27 +0330

    Index and Contact pages are added

--- DevFinProject/DevFin_MVC/Controllers/HomeController.cs
+++ DevFinProject/DevFin_MVC/Controllers/HomeController.cs
@@ -6,19 +6,16 @@ namespace DevFin_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
